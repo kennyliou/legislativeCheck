6b7b456e8640839b84d38e88325cb65fa219a81e
Kenny Liou <kenny2010ken@gmail.com>
2016-09-08 02:04:25 -0700

    delete this project

--- LegislativeCheck/src/com/k2c/model/NetworkCallbackInterface.java
+++
@@ -1,8 +0,0 @@
-package com.k2c.model;
-
-import org.json.JSONArray;
-
-public interface NetworkCallbackInterface {
-	public void NetworkCallback(JSONArray data);
-	public void NetworkPostExecute();
-}
